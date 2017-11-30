2acdc094824d16d8f43afb1d3b2089ceb2146bd8
PinkDeer <bunman@mail.ru>
2017-12-01 00:23:49 +0300

    Add Firms controller and views

---
+++ app/controllers/firms_controller.rb
@@ -0,0 +1,50 @@
+class FirmsController < ApplicationController
+
+  before_action :find_firm, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @firms = Firm.all
+  end
+
+  def show
+  end
+
+  def new
+    @firm = Firm.new
+  end
+
+  def create
+    @firm = Firm.new(firm_params)
+    if @firm.save
+			redirect_to @firm
+		else
+			render 'new'
+		end
+  end
+
+  def edit
+  end
+
+  def update
+    if @firm.update_attributes(firm_params)
+      redirect_to @firm
+    else
+      reder 'edit'
+    end
+  end
+
+  def destroy
+    @firm.destroy
+    redirect_to firms_path
+  end
+
+  private
+
+    def find_firm
+      @firm = Firm.find(params[:id])
+    end
+
+    def firm_params
+      params.require(:firm).permit(:name, :logo, :datestamp, :people)
+    end
+end
